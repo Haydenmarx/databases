ca0df3e3d6ed491c54a815cf8dba126df45ea27a
Hack Reactor Student <student@hackreactor.com>
2017-12-23 14:27:44 -0800

    Update schema with working users and messages tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,26 @@ CREATE DATABASE chat;
 
 USE chat;
 
+
+CREATE TABLE users (
+  id INT(11) AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(40) NOT NULL
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT(11) AUTO_INCREMENT PRIMARY KEY, 
+  user_id int,
+  message VARCHAR(280) NOT NULL, 
+  room VARCHAR(40) NOT NULL, 
+  posted DATE NOT NULL,
+  FOREIGN KEY (user_id) 
+    REFERENCES users(id)
+    ON DELETE CASCADE
 );
 
 /* Create other tables and define schemas for them here! */
 
 
-
-
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u student < server/schema.sql
  *  to create the database and the tables.*/
-
